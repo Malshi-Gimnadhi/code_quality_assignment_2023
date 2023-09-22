7e2dee4d3f19a12e203254c851ff7919dcdfc805
Malshi Gimnadhi <139608397+Malshi-Gimnadhi@users.noreply.github.com>
2023-09-22 10:54:12 +0000

    EG/2020/3942 Gimnadhi P.M.T. cORDING STANDARDS ASSIGNMENT

--- shop_calculator.py
+++ shop_calculator.py
@@ -1,97 +1,58 @@
 import csv
 
-discnts = {}
-taxes = {}
-prmtns = {}
-curr_rates = {}
-invntry = {}
+# Create dictionaries to store data
 
-def ld_discnts(file):
-    with open(file) as dscnt_file:
-        r = csv.reader(dscnt_file)
-        for rw in r:
-            discnts[rw[0].upper()] = float(rw[1])
+discounts = {}
+taxRates = {}
+promotions = {}
+currencyRates = {}
+inventory = {}
 
-def ld_taxes(file):
-    with open(file) as tax_file:
-        r = csv.reader(tax_file)
-        for rw in r:
-            taxes[rw[0].upper()] = float(rw[1])
+# Load discount data from CSV file
 
-def ld_prmtns(file):
-    with open(file) as prmtn_file:
-        r = csv.reader(prmtn_file)
-        for rw in r:
-            prmtns[rw[0]] = float(rw[1])
+def loadDiscounts(file):
+    with open(file) as discountFile:
+        reader = csv.reader(discountFile)
+        for row in reader:
+            discounts[row[0].upper()] = float(row[1])
 
-def ld_curr_rates(file):
-    with open(file) as currency_file:
-        r = csv.reader(currency_file)
-        for rw in r:
-            curr_rates[rw[0].upper()] = float(rw[1])
+# Load tax rate data from CSV file
 
-def ld_invntry(file):
-    with open(file) as inv_file:
-        r = csv.reader(inv_file)
-        for rw in r:
-            invntry[rw[0].upper()] = int(rw[1])
+def loadTaxRates(file):
+    with open(file) as taxFile:
+        reader = csv.reader(taxFile)
+        for row in reader:
+            taxRates[row[0].upper()] = float(row[1])
+
+# Load promotion data from CSV file
+
+def loadPromotions(file):
+    with open(file) as promotionFile:
+        reader = csv.reader(promotionFile)
+        for row in reader:
+            promotions[row[0]] = float(row[1])
+
+# Load currency rate data from CSV file
+
+def loadCurrencyRates(file):
+    with open(file) as currencyFile:
+        reader = csv.reader(currencyFile)
+        for row in reader:
+            currencyRates[row[0].upper()] = float(row[1])
+
+# Load inventory data from CSV file
+
+def loadInventory(file):
+    with open(file) as inventoryFile:
+        reader = csv.reader(inventoryFile)
+        for row in reader:
+            inventory[row[0].upper()] = int(row[1])
 
 def main():
 
-    ld_discnts('data/discounts.csv')
-    ld_taxes('data/tax_rates.csv')
-    ld_prmtns('data/promotions.csv')
-    ld_curr_rates('data/currency_rates.csv')
-    ld_invntry('data/inventory.csv')
+    # Load data from CSV files into dictionaries
 
-    with open('shopping_cart.csv') as file:
-        cart = {rw[0]: int(rw[1]) for rw in csv.reader(file)}
-
-    total_price_usd = 0
-
-    for itm, qty in cart.items():
-
-        itm = itm.upper()
-
-        if invntry.get(itm, 0) < qty:
-            print(f"Cannot proceed, insufficient inventory for {itm}")
-            return
-
-        promo = prmtns.get(itm)
-        if promo:
-            qty = qty - (qty // promo)
-        dscnt = discnts.get(itm, 0)
-        price = 1.0  
-        dscntd_price = price - (price * dscnt)
-        item_price = dscntd_price * qty
-        total_price_usd += item_price
-        invntry[itm] -= qty 
-
-    available_states = list(taxes.keys())
-    print("Enter your state. Available states are: ", ', '.join(available_states))
-    state = input()
-
-    tax_rate = taxes.get(state, 0)
-    tax_amt = total_price_usd * tax_rate
-
-    final_price_usd = total_price_usd + tax_amt
-
-    print("-------- SHOPPING CART SUMMARY --------")
-    print("Item   | Qty | Price")
-    print("-----------------------------")
-    for itm, qty in cart.items():
-        print(f"{itm.ljust(6)} | {str(qty).ljust(3)} | USD {dscntd_price * qty:.2f}")
-    print("-----------------------------")
-    print(f"Subtotal: USD {total_price_usd:.2f}")
-    print(f"Tax ({state}): USD {tax_amt:.2f}")
-    print(f"Total: USD {final_price_usd:.2f}")
-
-    available_currencies = list(curr_rates.keys())
-    selected_currency = input("Select currency for payment ({}): ".format(', '.join(available_currencies)))
-
-    final_price_currency = final_price_usd * curr_rates.get(selected_currency)
-
-    print(f"Total in {selected_currency}: {final_price_currency:.2f}")
-
-if __name__ == "__main__":
-    main()
+    loadDiscounts('data/discounts.csv')
+    loadTaxRates('data/taxRates.csv')
+    loadPromotions('data/promotions.csv')
+    loadCurrencyRates('data/currencyRates.csv
--- travel_cost_calculator.py
+++ travel_cost_calculator.py
@@ -1,49 +1,61 @@
-from csv import *
+from csv import reader
 
-a = {}
-b = {}
-c = {}
+# Create dictionaries to store the  data
 
-def lhr(file):  
-    with open(file) as h:
-        r = reader(h)
-        for row in r:
-            a[row[0]] = float(row[1])
+hotelRates = {}
+exchangeRates = {}
+flightCosts = {}
 
-def ler(file): 
-    with open(file) as e:
-        r = reader(e)
-        for row in r:
-            b[row[0].upper()] = float(row[1]) * 1 
+# Load data from CSV files into the dictionaries
 
-def lfr(file):
-    with open(file) as f:
-        r = reader(f)
-        for row in r:
-            c[row[0]] = float(row[1])
+def loadHotelRates(file):
+    with open(file) as hotelFile:
+        csv_reader = reader(hotelFile)
+        for row in csv_reader:
+            hotelRates[row[0]] = float(row[1])
+
+def load_exchangeRates(file):
+    with open(file) as exchangeFile:
+        csv_reader = reader(exchangeFile)
+        for row in csv_reader:
+            exchangeRates[row[0].upper()] = float(row[1])
+
+def load_flightCosts(file):
+    with open(file) as flightFile:
+        csv_reader = reader(flightFile)
+        for row in csv_reader:
+            flightCosts[row[0]] = float(row[1])
 
 def main():
-    lhr('data/hotel_rates.csv')
-    ler('data/exchange_rates.csv')
-    lfr('data/flight_costs.csv')
 
-    d = input("Enter your destination: ").upper()
+    # Load data from CSV files into the dictionaries
 
-    f = c.get(d, 0.0)
-    h = a.get(d, 0.0)
+H('data/hotelRates.csv')
+    load_exchangeRates('data/exchangeRates.csv')
+    load_flightCosts('data/flightCosts.csv')
+
+    # Get user input for destination,duration
+
+    destination = input("Enter your destination: ").upper()
+    flight_cost = flightCosts.get(destination, 0.0)
+    hotel_cost_per_day = hotelRates.get(destination, 0.0)
 
     days = int(input("Enter your stay duration in days: "))
-    h *= days
-    total = f + h
+    hotel_cost = hotel_cost_per_day * days
+    total_cost = flight_cost + hotel_cost
 
-    print(f"Flight cost: USD {f:.2f}")
-    print(f"Hotel cost for {days} days: USD {h:.2f}")
-    print(f"Total: USD {total:.2f}")
+    print(f"Flight cost: USD {flight_cost:.2f}")
+    print(f"Hotel cost for {days} days: USD {hotel_cost:.2f}")
+    print(f"Total: USD {total_cost:.2f}")
 
-    currency = input(f"Select your currency for final price estimation ({', '.join(b.keys())}): ")
+    # Get user input for the currency selection
 
-    p = total * b[currency]
-    print(f"Total in {currency}: {p:.2f}")
+    currencies = ', '.join(exchangeRates.keys())
+    selected_currency = input(f"Select your currency for final price estimation ({currencies}): ")
+
+    exchange_rate = exchangeRates.get(selected_currency, 1.0)
+    price_in_selected_currency = total_cost * exchange_rate
+    print(f"Total in {selected_currency}: {price_in_selected_currency:.2f}")
 
 if __name__ == "__main__":
     main()
